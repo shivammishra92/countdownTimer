578a9e0298fb602b57ee064523f617f64bef8a21
Shivam Mishra <shivam552mishra@gmail.com>
2024-01-05 18:54:31 +0530

    js file added

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-let endDate = "7th January, 2024 10:00 PM";
+let endDate = "7 January, 2024 10:00 AM";
 document.getElementById("end-Date").innerText = endDate;
+
+let inputs = document.querySelectorAll("input");
+
+function clock(){
+    let end = new Date(endDate);
+    let start = new Date();
+    let diff = (end - start)/1000;
+    //console.log(diff);
+
+    inputs[0].value = Math.floor(diff/(60*60*24));
+    inputs[1].value = Math.floor(diff/(60*60) % 24);
+    inputs[2].value = Math.floor(diff/(60) % 60);
+    inputs[3].value = Math.floor(diff % 60);
+
+}
+
+clock();
+
+setInterval(()=>{
+    clock()
+},1000);
